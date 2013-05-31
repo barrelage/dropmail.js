fbf89b04c27254dae0f1850e8af701c5e7d2de2d
Evan Owen <kainosnoema@gmail.com>
2013-05-30 20:41:47 -0700

    Rename Model#fetch to Model#find
    This matches fairly understood APIs and is just a little more usable.
    
    Signed-off-by: Tyler Smalley <tyler@gmail.com>
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- examples/node/client.js
+++ examples/node/client.js
@@ -4,7 +4,7 @@ Dropmail.Model.baseURL = "http://localhost:9292";
 Dropmail.Model.key = "w9GxuBn8ZeujTdIz1dSurEsmPkBbi1Wq2R53EW0mXY4";
 
 var user;
-Dropmail.User.fetch("105145576407562266", function(err, u) {
+Dropmail.User.find("105145576407562266", function(err, u) {
   if(err) return console.error(err);
   user = u;
   console.log(u);
