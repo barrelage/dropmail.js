494f688f0ddb41d425e54b9b6b21a103939d6106
Evan Owen <kainosnoema@gmail.com>
2013-06-14 03:55:41 -0700

    Add third build file for debugging with sourcemaps
    Leaves the non-debugging build file a little smaller, by default.
    
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- build.js
+++ build.js
@@ -2,18 +2,23 @@ var fs = require('fs')
   , browserify = require('browserify')
   , UglifyJS = require("uglify-js");
 
-var buildFileDebug = './build/dropmail.js'
-  , buildFile      = './build/dropmail.min.js'
-
-browserify()
+var bundler = browserify()
   .add('./browser')
   .ignore('buffer')
   .require('es5-shim')
   .require('js-base64')
-  .require('json2ify')
-  .bundle({ debug: true }, function(err, src) {
-  if (err) return console.error(err);
+  .require('json2ify');
 
-  fs.writeFileSync(buildFileDebug, src);
-  fs.writeFileSync(buildFile, UglifyJS.minify(src, { fromString: true }).code);
+var buildFileDebug = './build/dropmail.debug.js'
+  , buildFileMin   = './build/dropmail.min.js'
+  , buildFile      = './build/dropmail.js';
+
+bundler.bundle({ debug: true }, function(err, src) {
+  if (err) return console.error(err);
+  fs.writeFile(buildFileDebug, src);
+});
+
+bundler.bundle(function(err, src) {
+  fs.writeFile(buildFile, src);
+  fs.writeFile(buildFileMin, UglifyJS.minify(src, { fromString: true }).code);
 });
